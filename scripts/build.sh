dee3c3c608c7ec17dfe9bc878064dde977613056
assaf <445552+asssaf@users.noreply.github.com>
2026-07-02 22:02:41 -0700

    add support for x86_64 target

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 : ${IMAGE:=asssaf/vcore}
 : ${TARGET:=export-initramfs}
+: ${ARCH:=aarch64}
 : ${OUTPUT_TAR:=""}
 : ${OUTPUT_LOCAL:=""}
 
@@ -11,5 +12,6 @@ docker build -t ${IMAGE} \
 	--target=${TARGET} \
         ${OUTPUT_TAR:+--output "type=tar,dest=$OUTPUT_TAR"} \
         ${OUTPUT_LOCAL:+--output "type=local,dest=$OUTPUT_LOCAL"} \
+	--build-arg "ARCH=${ARCH}" \
 	-f docker/Dockerfile \
 	.
