b2d64a6b190b96549015e080ca4b2bbe90f81cb9
assaf <445552+asssaf@users.noreply.github.com>
2026-07-04 12:42:33 -0700

    arg for the busybox binpkgs source

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 : ${IMAGE:=asssaf/vcore}
 : ${TARGET:=export-initramfs}
 : ${ARCH:=aarch64}
+: ${BUSYBOX_BINPKGS:=""}
 : ${OUTPUT_TAR:=""}
 : ${OUTPUT_LOCAL:=""}
 
@@ -13,5 +14,6 @@ docker build -t ${IMAGE} \
         ${OUTPUT_TAR:+--output "type=tar,dest=$OUTPUT_TAR"} \
         ${OUTPUT_LOCAL:+--output "type=local,dest=$OUTPUT_LOCAL"} \
 	--build-arg "ARCH=${ARCH}" \
+	${BUSYBOX_BINPKGS:+--build-arg "BUSYBOX_BINPKGS=${BUSYBOX_BINPKGS}"} \
 	-f docker/Dockerfile \
 	.
